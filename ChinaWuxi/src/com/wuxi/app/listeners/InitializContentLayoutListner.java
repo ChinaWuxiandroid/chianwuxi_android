9ef79c819ad3131c43eb02cb5a2ed2920798200a
wanglu <wanglu0919@163.com>
2013-06-21 17:30:46 +0800

    初始化界面监听

---
+++ ChinaWuxi/src/com/wuxi/app/listeners/InitializContentLayoutListner.java
@@ -0,0 +1,19 @@
+package com.wuxi.app.listeners;
+
+import com.wuxi.app.BaseFragment;
+
+/**
+ * 内容界面绑定监听器
+ * 
+ * @author wanglu
+ * 
+ */
+public interface InitializContentLayoutListner {
+
+	/**
+	 * 绑定界面
+	 * @param fragment
+	 */
+	
+	public void bindContentLayout(BaseFragment fragment);
+}
