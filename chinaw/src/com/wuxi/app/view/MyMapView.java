f594cc215b9f32340e14425127b47df49e0eadf4
wanglu <wanglu0919@163.com>
2013-06-29 16:52:25 +0800

    城市地图开发基本完成

---
+++ chinaw/src/com/wuxi/app/view/MyMapView.java
@@ -0,0 +1,33 @@
+package com.wuxi.app.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.PopupOverlay;
+
+public class MyMapView extends MapView {
+
+public static PopupOverlay   pop  = null;//弹出泡泡图层，浏览节点时使用
+	
+	public MyMapView(Context context) {
+		super(context);
+	}
+	public MyMapView(Context context, AttributeSet attrs){
+		super(context,attrs);
+	}
+	public MyMapView(Context context, AttributeSet attrs, int defStyle){
+		super(context, attrs, defStyle);
+	}
+	@Override
+    public boolean onTouchEvent(MotionEvent event){
+		if (!super.onTouchEvent(event)){
+			//消隐泡泡
+			if (pop != null && event.getAction() == MotionEvent.ACTION_UP)
+				pop.hidePop();
+		}
+		return true;
+	}
+
+}
