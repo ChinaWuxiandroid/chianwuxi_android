d6d4f4767addd6cdadcefb3dfc74fb9557c3bd51
wanglu <wanglu0919@163.com>
2013-07-03 17:24:56 +0800

    领导之窗

---
+++ chinaw/src/com/wuxi/app/fragment/LeaderWindowFragment.java
@@ -0,0 +1,19 @@
+package com.wuxi.app.fragment;
+
+import com.wuxi.app.BaseFragment;
+
+/**
+ * 
+ * @author wanglu 泰得利通
+ * 领导之窗fragment
+ *
+ */
+public class LeaderWindowFragment extends BaseFragment {
+
+	
+	
+	
+	
+	
+	
+}
